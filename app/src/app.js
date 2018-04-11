0c2b60fc59797d684e5f08935e2b78a8519bc28b
Andrej Premrn <andrej.premrn@gmail.com>
2018-04-11 15:56:53 +0200

    Initialized a slider instance for dev purposes

--- app/src/app.js
+++ app/src/app.js
@@ -1,1 +1,10 @@
-console.log('test');
+import CircularSlider from './CircularSlider';
+
+var c = window.c = new CircularSlider({
+  container: document.getElementById('container'),
+  min: 0,
+  max: 200,
+  step: 20,
+  color: 'red',
+  radius: 200
+});
