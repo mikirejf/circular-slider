37d02fd7e30e6154cd5bc0972a42b32d6e9bba7c
Andrej Premrn <andrej.premrn@gmail.com>
2018-04-11 08:32:36 +0200

    Changed the host

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    host: '0.0.0.0',
     contentBase: appPath,
     watchContentBase: true,
     clientLogLevel: 'none'
