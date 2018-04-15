e24dc0c704b561e929758f16f2854639e4713f3c
Andrej Premrn <andrej.premrn@gmail.com>
2018-04-15 11:07:36 +0200

    Added initialization of multiple sliders

--- app/src/app.js
+++ app/src/app.js
@@ -1,10 +1,24 @@
 import CircularSlider from './CircularSlider';
 
-var c = window.c = new CircularSlider({
-  container: document.getElementById('container'),
-  min: 0,
-  max: 200,
-  step: 20,
-  color: 'red',
-  radius: 200
+const data = [
+  { label: 'Transportation', color: 'purple', min: 0, max: 1000, step: 10 },
+  { label: 'Food', color: 'blue', min: 0, max: 1000, step: 10 },
+  { label: 'Insurance', color: 'green', min: 0, max: 700, step: 5 },
+  { label: 'Entertainment', color: 'orange', min: 0, max: 600, step: 5 },
+  { label: 'Health care', color: 'red', min: 0, max: 500, step: 10 },
+];
+
+const container = document.getElementById('container');
+
+data.forEach((record, i) => {
+  const props = {
+    container: container,
+    min: record.min,
+    max: record.max,
+    step: record.step,
+    color: record.color,
+    radius: 400 - (i * 80)
+  };
+
+  new CircularSlider(props);
 });
