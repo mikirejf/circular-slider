470670af87356ace6f5a6f6814b7e592e79bee01
Andrej Premrn <andrej.premrn@gmail.com>
2018-04-15 15:45:14 +0200

    Built the app's layout

--- app/src/app.js
+++ app/src/app.js
@@ -3,12 +3,12 @@ import CircularSlider from './CircularSlider';
 const data = [
   { label: 'Transportation', color: 'purple', min: 0, max: 1000, step: 10 },
   { label: 'Food', color: 'blue', min: 0, max: 1000, step: 10 },
-  { label: 'Insurance', color: 'green', min: 0, max: 700, step: 5 },
-  { label: 'Entertainment', color: 'orange', min: 0, max: 600, step: 5 },
+  { label: 'Insurance', color: 'green', min: 0, max: 700, step: 10 },
+  { label: 'Entertainment', color: 'orange', min: 0, max: 600, step: 10 },
   { label: 'Health care', color: 'red', min: 0, max: 500, step: 10 },
 ];
 
-const container = document.getElementById('container');
+const container = document.querySelector('.sliders');
 
 data.forEach((record, i) => {
   const props = {
@@ -17,7 +17,7 @@ data.forEach((record, i) => {
     max: record.max,
     step: record.step,
     color: record.color,
-    radius: 400 - (i * 80)
+    radius: 350 - (i * 60)
   };
 
   new CircularSlider(props);
