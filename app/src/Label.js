2be6ef69129ec3d13cc2f1aaa525a15f6c103f89
Andrej Premrn <andrej.premrn@gmail.com>
2018-04-16 18:23:43 +0200

    Added basic LAbel component and some logic to render labels

--- app/src/Label.js
+++ app/src/Label.js
@@ -1,7 +1,11 @@
-class Label {
+export default class Label {
   constructor(props) {
     this.props = props;
     this.val = '';
+    this.styles = null;
+
+    this.defineStyles();
+    this.render();
   }
 
   set value (val) {
@@ -14,16 +18,48 @@ class Label {
     return this.val;
   }
 
+  defineStyles() {
+    const lighterColor = (this.props.color & 0x7f7f7f) << 1;
+
+    this.styles = {
+      element: {
+        width: '400px',
+        'display': 'flex'
+      },
+      value: {
+        width: '100px',
+        'font-family': 'sans-serif',
+        'font-size': '45px'
+      },
+      colorLabel: {
+        background: `linear-gradient(${lighterColor}, ${this.props.color})`,
+        width: '40px',
+        height: '40px'
+      },
+      label: {
+        width: '40px'
+      }
+    }
+  }
+
   render() {
     const element = document.createElement('div');
     const value = document.createElement('span');
-    const color = document.createElement('span');
+    const colorLabel = document.createElement('span');
     const label = document.createElement('span');
 
+    label.textContent = this.props.label;
+    
+    Object.assign(element.style, this.styles.element);
+    Object.assign(value.style, this.styles.value);
+    Object.assign(colorLabel.style, this.styles.colorLabel);
+    Object.assign(label.style, this.styles.label);
+    
     element.appendChild(value);
-    element.appendChild(color);
+    element.appendChild(colorLabel);
     element.appendChild(label);
-
+    
+    this.refs = { value };
     this.props.container.appendChild(element);
   }
 }
--- app/src/app.js
+++ app/src/app.js
@@ -1,18 +1,20 @@
 import CircularSlider from './CircularSlider';
+import Label from './Label';
 
 const data = [
-  { label: 'Transportation', color: 'purple', min: 0, max: 1000, step: 10 },
-  { label: 'Food', color: 'blue', min: 0, max: 1000, step: 10 },
-  { label: 'Insurance', color: 'green', min: 0, max: 700, step: 10 },
-  { label: 'Entertainment', color: 'orange', min: 0, max: 600, step: 10 },
-  { label: 'Health care', color: 'red', min: 0, max: 500, step: 10 },
+  { label: 'Transportation', color: '#674478', min: 0, max: 400, step: 100 },
+  { label: 'Food', color: '#0078c1', min: 300, max: 1000, step: 10 },
+  { label: 'Insurance', color: '#00a000', min: 0, max: 700, step: 10 },
+  { label: 'Entertainment', color: '#fe8130', min: 0, max: 600, step: 10 },
+  { label: 'Health care', color: '#ff3335', min: 0, max: 500, step: 10 },
 ];
 
-const container = document.querySelector('.sliders');
+const sliderContainer = document.querySelector('.sliders');
+const labelContainer = document.querySelector('.labels');
 
 data.forEach((record, i) => {
-  const props = {
-    container: container,
+  const sliderProps = {
+    container: sliderContainer,
     min: record.min,
     max: record.max,
     step: record.step,
@@ -20,5 +22,18 @@ data.forEach((record, i) => {
     radius: 350 - (i * 60)
   };
 
-  new CircularSlider(props);
+  const labelProps = {
+    container: labelContainer,
+    color: record.color,
+    label: record.label
+  };
+
+  const slider = new CircularSlider(sliderProps);
+  const label = new Label(labelProps);
+
+  label.value = `$${slider.value}`;
+
+  slider.on('value-changed', (val) => {
+    label.value = `$${val}`;
+  });
 });
