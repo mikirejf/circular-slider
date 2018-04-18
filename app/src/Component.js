9be02773521354df2f98ef45c9b8f5e400ec1cb5
Andrej Premrn <andrej.premrn@gmail.com>
2018-04-18 12:30:48 +0200

    Moved 'EventEmitter' with 'Component'
    To avoid writing code to support multiple inheritance, I just simply merged the two
    classes.

--- app/src/Component.js
+++ app/src/Component.js
@@ -4,6 +4,7 @@ export default class Component extends Template {
   constructor() {
     super();
     
+    this.listeners = {};
     this.props = {};
     this.state = {};
   }
@@ -11,5 +12,22 @@ export default class Component extends Template {
   setState(nextState) {
     this.state = Object.assign({}, this.state, nextState);
     this.update();
+  }
+
+  emit(event) {
+    if (this.listeners[event]) {
+      // Remove the `event` argument
+      const args = [].slice.call(arguments, 1);
+
+      this.listeners[event].forEach(function (cb) {
+        // TODO: maybe executine in the context of `this`?
+        cb.apply(window, args);
+      });
+    }
+  }
+
+  on(event, callback) {
+    this.listeners[event] = this.listeners[event] || [];
+    this.listeners[event].push(callback);
   }
 }
