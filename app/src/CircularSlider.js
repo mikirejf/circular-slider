6347ef10b6953351850cfd242fb8acb55c09c114
Andrej Premrn <andrej.premrn@gmail.com>
2018-04-19 16:07:51 +0200

    Refactored to the new templating system

--- app/src/CircularSlider.js
+++ app/src/CircularSlider.js
@@ -50,140 +50,16 @@ export default class CircularSlider extends Component {
     const uid = getGUID();
     const size = this.props.radius * 2;
     const knobRadius = this.STROKE_WIDTH / 2 + this.KNOB_OVERFLOW;
-
-    /* const svg = createSVGDOMElement('svg', {
-      width: size,
-      height: size,
-      'pointer-events': 'none',
-      style: `padding: ${this.KNOB_OVERFLOW + 2}px; position: absolute;`
-    });
-    // Promote to its own layer, to avoid paint phase
-    // TODO: check for support
-    svg.style.willChange = 'transform';
-
-    const mask = createSVGDOMElement('mask', { id: uid });
-
-    const maskOuterCircke = createSVGDOMElement('circle', {
-      cx: this.props.radius,
-      cy: this.props.radius,
-      r: this.props.radius,
-      fill: 'white'
-    });
-
-    const maskInnerCircle = createSVGDOMElement('circle', {
-      cx: this.props.radius,
-      cy: this.props.radius,
-      r: this.props.radius - this.STROKE_WIDTH,
-      fill: 'black'
-    });
-
-    const knob = createSVGDOMElement('circle', {
-      cx: this.props.radius,
-      cy: knobRadius,
-      r: knobRadius + this.KNOB_OVERFLOW,
-      stroke: 'black',
-      fill: 'white',
-      'pointer-events': 'all'
-    });
-
-    const clickLayer = createSVGDOMElement('circle', {
-      cx: this.props.radius,
-      cy: this.props.radius,
-      r: this.trueRadius,
-      'stroke-width': 50,
-      stroke: 'transparent',
-      fill: 'none',
-      'pointer-events': 'stroke'
-    });
-
-    const rect = createSVGDOMElement('rect', {
-      x: 0,
-      y: 0,
-      width: size + 6,
-      height: size + 6,
-      fill: '#babdc1',
-      mask: `url(#${uid})`
-    });
-
-    const arc = createSVGDOMElement('path', {
-      stroke: this.props.color,
-      opacity: 0.7,
-      fill: 'none',
-      'stroke-width': this.STROKE_WIDTH 
-    });
-
-    const group = createSVGDOMElement('g');
-
-    mask.appendChild(maskOuterCircke);
-    mask.appendChild(maskInnerCircle);
-
-    for (let i = 0; i < this.numOfSteps; i++) {
-      const step = polarToCartesian(
-        this.props.radius,
-        this.props.radius,
-        this.props.radius,
-        degreesToRadians(i * this.stepAngle - 90)
-      );
-      
-      mask.appendChild(createSVGDOMElement('line', {
-        x1: this.props.radius,
-        x2: step.x,
-        y1: this.props.radius,
-        y2: step.y,
-        stroke: 'black',
-        'stroke-width': 4
-      }));
-    }; */
-
-    // When you are rotating an element, its top position is calculated based on
-    // the highest edge of the current rotation state. To get the correct
-    // offset values of the rotating svg, we are wrapping it in a non rotating
-    // wrapper and calculating offsets from it.
-/*     const wrapper = document.createElement('div');
-    
-    Object.assign(wrapper.style, {
-      width: `${size}px`,
-      height: `${size}px`,
-      position: 'absolute'
-    });
-
-    wrapper.appendChild(svg);
-    svg.appendChild(mask);
-    svg.appendChild(rect)
-    svg.appendChild(arc);
-    svg.appendChild(group);
-    group.appendChild(clickLayer);
-    group.appendChild(knob);
-    
-    this.refs = { svg, knob, arc, clickLayer, group, wrapper };
-    this.props.container.appendChild(wrapper); */
-
     const nodeStyle = {
       width: `${size}px`,
       height: `${size}px`,
       position: 'absolute'
     };
-
     const svgStyle = {
       'pointer-events': 'none',
       padding: '4px',
       'will-change': 'transform'
     };
-
-
-    /* const maskOuterCircke = createSVGDOMElement('circle', {
-      cx: this.props.radius,
-      cy: this.props.radius,
-      r: this.props.radius,
-      fill: 'white'
-    });
-
-    const maskInnerCircle = createSVGDOMElement('circle', {
-      cx: this.props.radius,
-      cy: this.props.radius,
-      r: this.props.radius - this.STROKE_WIDTH,
-      fill: 'black'
-    }); */
 
     this.template`
       <div ref="wrapper" style=${nodeStyle}>
@@ -213,7 +89,7 @@ export default class CircularSlider extends Component {
           </rect>
           <path
             ref="arc" 
-            stroke="#ff3335" 
+            stroke="${this.props.color}" 
             opacity="0.7" 
             fill="none" 
             stroke-width="${this.STROKE_WIDTH}" 
@@ -261,8 +137,8 @@ export default class CircularSlider extends Component {
       maskLines.push(`
         <line 
           x1=${this.props.radius}
-          y1=${stepPoint.x} 
-          x2=${this.props.radius} 
+          y1=${this.props.radius} 
+          x2=${stepPoint.x} 
           y2=${stepPoint.y} 
           stroke="black" 
           stroke-width="4">
